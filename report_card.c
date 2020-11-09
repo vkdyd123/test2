392d46f0f8923bbfce0560b0c91c07aedb896b48
vkdyd123 <vkdyd124@naver.com>
2020-11-09 19:27:35 +0900

    report card: Add base code

--- report_card.c
+++ report_card.c
@@ -2,9 +2,4 @@
 
 void main(){
 	printf("This is Report Cards.c\n");
-	printf("what is the difference ? \n");
-	printf("Math: 90 \n");
-	printf("English: 100 \n");
-	printf("Sum grade: 190 \n");
-	printf("Average grade: 95 \n");
 }
